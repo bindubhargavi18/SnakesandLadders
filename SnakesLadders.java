c17f87f34197e2504f161909ae9c0cacd7ac6913
bindubhargavi18 <bindubhargavi568@gmail.com>
2021-07-29 00:47:30 +0530

    Added:snakes and ladders

---
+++ SnakesLadders.java
@@ -0,0 +1,13 @@
+package snakeladder;
+
+public class SnakesLadders 
+{
+	static final int startPosition=0;
+	
+	public static void main(String[] args) 
+	{
+		int playerPosition=startPosition;
+		System.out.println("The starting position of player is: "+playerPosition);
+	}
+
+}
