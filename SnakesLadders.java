027f572bfa2cfb881adf572ad0f69ee3592c61df
bindubhargavi18 <bindubhargavi568@gmail.com>
2021-07-29 01:05:17 +0530

    Added:rolling a dice

--- SnakesLadders.java
+++ SnakesLadders.java
@@ -1,4 +1,5 @@
 package snakeladder;
+import java.util.*;
 
 public class SnakesLadders 
 {
@@ -7,8 +8,10 @@ public class SnakesLadders
 	public static void main(String[] args) 
 	{
 		int playerPosition=startPosition;
-		
+		Random r=new Random();
+		int rollDice=r.nextInt(6)+1;
 		System.out.println("The starting position of player is: "+playerPosition);
+		System.out.println("Number that player got on dice is: " +rollDice);
 	}
 
 }
